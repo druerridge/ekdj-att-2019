6d98c3938a29265af850f3e427ba44c3b4e0d9ec
druerridge <druerridge@gmail.com>
2019-10-05 19:29:01 -0700

    Added a 'crosshair' behavior that highlights any poiSphere and when space is clicked, pulls up the associated video in it's properties

--- Multistream/Assets/Scripts/VideoPOI.cs
+++ Multistream/Assets/Scripts/VideoPOI.cs
@@ -1,39 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Mapbox.Unity.MeshGeneration.Interfaces;
 using UnityEngine;
 using UnityEngine.Video;
+using System.Linq;
 
 public class VideoPOI : MonoBehaviour, IFeaturePropertySettable
 {
-    public string videoUrl;
     public Vector3 videoPlayerPosition;
     public GameObject videoPlayerPrefab;
-    private static bool showedIt;
+    public GameObject highlightSphere;
+    private static Dictionary<string, object> defaultProps = new Dictionary<string, object>()
+    {
+        { "videoUrl1", "http://2115b16a.ngrok.io/videos/videoplayback1.mp4" },
+        { "videoUrl2", "http://2115b16a.ngrok.io/videos/videoplayback1.mp4" }
+    };
+    private Dictionary<string, object> poiProperties = defaultProps;
+    internal bool highlighted;
+    private bool startedPlayback;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !showedIt)
+        if (highlighted)
         {
-            showedIt = true;
-            Debug.Log("Happened");
+            highlightSphere.SetActive(true);
+        }
+        else
+        {
+            highlightSphere.SetActive(false);
+        }
+    }
+
+    internal void StartVideo()
+    {
+        if (!startedPlayback)
+        {
+            startedPlayback = true;
+            var jsonString = JsonUtility.ToJson(poiProperties);
+            Debug.Log("poiProperties: " + jsonString);
+            string videoUrl = (string)poiProperties["videoUrl1"];
+            Debug.Log("Trying to play video: " + videoUrl);
             var videoPlayer = Instantiate(videoPlayerPrefab).GetComponent<VideoPlayer>();
             videoPlayer.gameObject.transform.SetPositionAndRotation(videoPlayerPosition, videoPlayer.gameObject.transform.rotation);
             videoPlayer.url = videoUrl;
-            //videoPlayer.Play();
         }
     }
 
     public void Set(Dictionary<string, object> props)
     {
         var jsonString = JsonUtility.ToJson(props);
-        Debug.Log("Props: " + jsonString);
+        Debug.Log("Props (" + props.Count  +"): " + jsonString);
+        poiProperties = poiProperties.Union(props).ToDictionary(k => k.Key, v => v.Value);
     }
 }
